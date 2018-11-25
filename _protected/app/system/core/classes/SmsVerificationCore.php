e5b76d20df1acd711137d1ec2dad7794d7b49488
Pierre-Henry Soria <hi@ph7.me>
2018-11-25 13:06:00 +1300

    Add SmsVerificationCore class

---
+++ _protected/app/system/core/classes/SmsVerificationCore.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author         Pierre-Henry Soria <hello@ph7cms.com>
+ * @copyright      (c) 2018, Pierre-Henry Soria. All Rights Reserved.
+ * @license        GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
+ * @package        PH7 / App / System / Core / Class
+ */
+
+namespace PH7;
+
+class SmsVerificationCore
+{
+    const PROFILE_ID_SESS_NAME = 'sms_verification_profile_id';
+    const USER_EMAIL_SESS_NAME = 'sms_verification_user_email';
+    const PHONE_NUMBER_SESS_NAME = 'user_phone_number';
+}
